59948e3eb47f90bb1491b05a6c974b4ab2f750f3
AhmedEmaad18 <ahmed.emadd18@gmail.com>
2023-04-26 23:56:43 +0200

    newrouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { ElectristyDataComponent } from './electristy-data/electristy-data.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { WaterDataComponent } from './water-data/water-data.component';
+import { TelephoneDataComponent } from './telephone-data/telephone-data.component';
 import { ElectInvoiceComponent } from './elect-invoice/elect-invoice.component';
 import { WaterInvoiceComponent } from './water-invoice/water-invoice.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
@@ -14,8 +18,11 @@ const routes: Routes = [
  { path: 'elect', component: ElectInvoiceComponent },
  { path: 'water', component: WaterInvoiceComponent },
  { path: 'slide', component: SlideshowComponent },
- {path:'userhome',component:UserDashboardComponent}
-
+ {path:'userhome',component:UserDashboardComponent},
+ {path:'toAdminElectericity',component:ElectristyDataComponent},
+ {path:'toAdminDashboard',component:AdminDashboardComponent},
+ {path:'toAdminWater',component:WaterDataComponent},
+ {path:'toAdminTelephone',component:TelephoneDataComponent}
 ];
 
 @NgModule({
